505d7dd71fa4ee085b9e9b95c6b4a7ad9e57048a
sformichella <sformichella@gmail.com>
2020-12-15 11:42:37 -0800

    new find by id test

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS artists CASCADE;
 DROP TABLE IF EXISTS albums CASCADE;
-DROP TABLE IF EXISTS arists_albums;
+DROP TABLE IF EXISTS artists_albums;
 
 CREATE TABLE artists (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -12,7 +12,7 @@ CREATE TABLE albums (
   album TEXT NOT NULL
 );
 
-CREATE TABLE arists_albums (
+CREATE TABLE artists_albums (
   artist_id BIGINT references artists(id),
   album_id BIGINT references albums(id),
   PRIMARY KEY(artist_id, album_id)
